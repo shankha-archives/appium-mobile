0417ac351d6711e02fba068d63c4eb015d899746
Stanley Griggs II <sgriggs@frontlineed.com>
2020-02-24 17:33:00 -0500

    WIP: CREATE WEB ELEMENT ACTIONS & TODO LIST
    Made actions when switched to web context

--- src/main/java/com/appiumframework/core/BaseTest.java
+++ src/main/java/com/appiumframework/core/BaseTest.java
@@ -5,6 +5,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,6 +15,7 @@ import org.testng.annotations.BeforeTest;
 
 import java.net.URL;
 import java.util.Properties;
+import java.util.Set;
 
 public class BaseTest {
     protected static AppiumDriver driver;
@@ -41,12 +43,14 @@ public class BaseTest {
 
             driver = new IOSDriver(url, desiredCapabilities);
             String sessionId = driver.getSessionId().toString();
+
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
     }
 
+    //MobileElement Actions:
     public void waitForVisibility(MobileElement e) {
         WebDriverWait wait = new WebDriverWait(driver, TestUtils.WAIT);
         wait.until(ExpectedConditions.visibilityOf(e));
@@ -67,8 +71,67 @@ public class BaseTest {
         e.getAttribute(attribute);
     }
 
+    //WebElement Actions:
+    public void switchContextToNative() {
+        Set<String> contextNames = driver.getContextHandles();
+        driver.context((String) contextNames.toArray()[1]);
+        System.out.println("THE CONTEXT IS NOW SET TO " + driver.getContext());
+    }
+
+    public void waitForVisibilityWebElement(WebElement e) {
+        WebDriverWait wait = new WebDriverWait(driver, TestUtils.WAIT);
+        wait.until(ExpectedConditions.visibilityOf(e));
+    }
+
+    public void sendKeysWebElement(WebElement e, String txt) {
+        waitForVisibilityWebElement(e);
+        e.sendKeys(txt);
+    }
+
+    public void clickWebElement(WebElement e) {
+        waitForVisibilityWebElement(e);
+        e.click();
+    }
+
+    public void switchContextToWebview() throws InterruptedException {
+        changeDriverContextToWeb(driver);
+        System.out.println("THE CONTEXT IS NOW SET TO " + driver.getContext());
+    }
+
+    public static void changeDriverContextToWeb(AppiumDriver driver) throws InterruptedException {
+        //allow time for the webView to load completely
+        Thread.sleep(5000);
+        //TODO: Find a way to wait for the webview to load without using Thread.sleep
+
+        //gets all of the avilable contexts
+        Set<String> contextNames = driver.getContextHandles();
+        //Switch to WebView Context
+        driver.context((String) contextNames.toArray()[1]);
+        System.out.println(contextNames);
+    }
     @AfterTest
     public void afterTest() {
         driver.quit();
     }
 }
+
+//TODO: STEPS BELOW:
+/*
+* Change Context to webview
+*   -Make Function in Base Class    [x]
+* Wait For Visibility of LOGIN      [x]
+* Wait For Visibility of PASSWORD   [x]
+* Send Password to LOGIN            [x]
+* Send Password to PASSWORD         [x]
+*
+*   TESTING FLOWS
+*       CORRECT     UN/PW           [x]
+*       INCORRECT   UN/PW           [ ]
+*
+* Upon Logging in Switch to Dashboard Class switch the context back to Mobile   [ ]
+*
+* Create Dashboard Page Class [ ]
+* Create Dashboard Test Class [ ]
+*   MISC
+*       USE XCODE SAFARI INSPECTOR TO GET THE ID OF INCORRECT PASSWORD BUBBLE FOR TEST ASSERTIONS [ ]
+* */
