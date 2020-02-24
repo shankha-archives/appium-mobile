7ae016dbafcf7b3ddd5494859d55985f17c3d4fb
Stanley Griggs II <sgriggs@frontlineed.com>
2020-02-24 17:37:15 -0500

    WIP: CREATE DASHBOARD PAGE CLASS AND REMOVED UNUSED CLASS
    Made a placeholder comment out class to start building out the Dashboard page. Also removed unused test1 class from when i was learning the framework initially.

---
+++ src/main/java/com/appiumframework/pages/DashboardPage.java
@@ -0,0 +1,26 @@
+//package com.appiumframework.pages;
+//
+//import com.appiumframework.core.BaseTest;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
+//
+//public class DashboardPage extends BaseTest {
+//
+//
+//
+//    public DashboardPage enterUserName() {
+//        switchContexttoWebview();
+//        sendKeysWebElement(username,"Prod01");
+//        return this;
+//    }
+//
+//    public DashboardPage enterPassword() {
+//        sendKeysWebElement(password,"Password1!");
+//        return this;
+//    }
+//
+//    public DashboardPage tapLoginBtn() {
+//        clickWebElement(loginBtn);
+//        return this;
+//    }
+//}
