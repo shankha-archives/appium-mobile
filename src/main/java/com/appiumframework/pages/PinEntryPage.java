a2637e587aa81eb749bbf3b36897e194cbf4e9d3
Stanley Griggs II <sgriggs@frontlineed.com>
2020-03-03 15:59:14 -0500

    WIP: PIN ENTRY TEST ADDITION & PAGE REFORMATTING
    Added incorrect PIN scenario and added all of the button elements to the page class.

--- src/main/java/com/appiumframework/pages/PinEntryPage.java
+++ src/main/java/com/appiumframework/pages/PinEntryPage.java
@@ -5,10 +5,24 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 
 public class PinEntryPage extends BaseTest {
-    @iOSXCUITFindBy(id = "7_button") private MobileElement sevenBtn;
+    @iOSXCUITFindBy(id = "1_button") private MobileElement oneBtn;
+    @iOSXCUITFindBy(id = "2_button") private MobileElement twoBtn;
     @iOSXCUITFindBy(id = "3_button") private MobileElement threeBtn;
-    @iOSXCUITFindBy(id = "5_button") private MobileElement fiveBtn;
     @iOSXCUITFindBy(id = "4_button") private MobileElement fourBtn;
+    @iOSXCUITFindBy(id = "5_button") private MobileElement fiveBtn;
+    @iOSXCUITFindBy(id = "6_button") private MobileElement sixBtn;
+    @iOSXCUITFindBy(id = "7_button") private MobileElement sevenBtn;
+    @iOSXCUITFindBy(id = "8_button") private MobileElement eightBtn;
+    @iOSXCUITFindBy(id = "9_button") private MobileElement nineBtn;
+    @iOSXCUITFindBy(id = "0_button") private MobileElement zeroBtn;
+
+    public PinEntryPage pressIncorrectPinBtns() {
+        click(oneBtn);
+        click(nineBtn);
+        click(oneBtn);
+        click(oneBtn);
+        return this;
+    }
 
     public LoginPage pressPinBtns() {
         click(sevenBtn);
