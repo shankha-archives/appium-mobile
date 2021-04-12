8ab5a73aacd1cac4a09bd525b9da5b3a0faa81ee
rupa310598 <rupa.kumari@frontlineed.com>
2021-04-12 10:43:13 +0530

    Added jobs and login features

--- src/test/java/mobile/frontline/runners/MyRunnerTest.java
+++ src/test/java/mobile/frontline/runners/MyRunnerTest.java
@@ -20,7 +20,7 @@ import io.cucumber.junit.CucumberOptions;
         ,dryRun=false
         ,monochrome=true
         ,strict=true
-        ,tags = {"@MOB-4261"}
+        ,tags = {"@jobs"}
 )
 
 public class MyRunnerTest {
